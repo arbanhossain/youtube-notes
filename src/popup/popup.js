5abde0404cb39c246106fedacbef9eaf3274dfb6
arbanhossain <arbanhossain@gmail.com>
2020-05-07 11:22:13 +0600

    note is now saved in localstorage

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,6 +1,23 @@
 let submitButton = document.getElementById('commit');
 let x = null;
 
+let SRC = '';
+
+const init_notepad = () => {
+  //let pad = localStorage.getItem();
+  chrome.tabs.executeScript({
+    code: `x = window.location.href;x;`
+  }, (src) => {
+    SRC = src[0]
+    console.log(SRC);
+    SRC = SRC.replace("https://www.youtube.com/watch?v=", "");
+    console.log(`id is ${SRC}`);
+    if(localStorage.getItem(SRC) == null){
+      localStorage.setItem(SRC, ``);
+    }
+  });
+}
+
 const clearNotepad = () => {
   document.getElementById('note').value = '';
 }
@@ -11,7 +28,9 @@ const saveNote = (data) => {
 }
 
 const addToNote = (time, note) => {
-  console.log(`Time=${time}, Note=${note}`);
+  let text = localStorage.getItem(SRC) + `Time=${time}, Note=${note}\n`;
+  console.log(text);
+  localStorage.setItem(SRC, text)
   clearNotepad();
 }
 
@@ -23,3 +42,5 @@ submitButton.onclick = (e) => {
     addToNote(timestamp, document.getElementById('note').value);
   });
 }
+
+init_notepad();
